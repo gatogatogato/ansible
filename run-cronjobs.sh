723901a58f30c001287ce115ea99a1e6bfc26294
gatogatogato <tobias.gaulke@gmail.com>
2024-05-09 11:47:47 +0200

    Add new stuff

--- run-cronjobs.sh
+++ run-cronjobs.sh
@@ -5,7 +5,7 @@ PARMS=""
 
 # TEST CHANGE
 
-ACTIONS="cronjobs_webservers"
+ACTIONS="cronjobs_webservers cronjobs_flickrservers"
 
 for ACTION in ${ACTIONS}; do
 	ansible-playbook ${RUNDIR}/${ACTION}.yaml ${PARMS} -i ${RUNDIR}/${INVENTORY}
