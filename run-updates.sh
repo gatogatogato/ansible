11e5e4554e490caf71208088a377fd3db480f0a7
gatogatogato <tobias.gaulke@gmail.com>
2024-05-11 21:29:10 +0200

    Add omz updates

--- run-updates.sh
+++ run-updates.sh
@@ -3,7 +3,7 @@ RUNDIR=~transport/ansible/
 INVENTORY=inventory.yaml
 PARMS=""
 
-ACTIONS="update_all_apt update_all_gem update_all_micro update_webservers_snap"
+ACTIONS="update_all_apt update_all_gem update_all_micro update_all_omz update_webservers_snap"
 
 for ACTION in ${ACTIONS}; do
 	ansible-playbook ${RUNDIR}/${ACTION}.yaml ${PARMS} -i ${RUNDIR}/${INVENTORY}
