09e4619f1c02fd4617c2f09d37da057b401ec6c3
gatogatogato <tobias.gaulke@gmail.com>
2024-12-16 12:31:39 +0100

    Add message

--- run-shutdown-unproductive.sh
+++ run-shutdown-unproductive.sh
@@ -3,6 +3,12 @@ RUNDIR=~transport/ansible/
 INVENTORY=inventory.yaml
 PARMS=""
 
+echo "* * * * * * * * * * * * * * * * * * * * * * * *"
+echo "*                                             *"
+echo "*    Shutting down non-productive servers!    *"
+echo "*                                             *"
+echo "* * * * * * * * * * * * * * * * * * * * * * * *"
+
 ACTIONS="shutdown_unproductive"
 
 for ACTION in ${ACTIONS}; do
